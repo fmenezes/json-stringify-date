31c5abca8b223bc264c8696d1232ce059618e551
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2014-04-04 17:03:45 -0300

    "JSLinting" the code

--- test.js
+++ test.js
@@ -1,27 +1,25 @@
+/*jslint node: true */
 var stringifyDate = require('./index');
 stringifyDate.setOptions({utc: true});
 
 var obj = {
-	d: new Date(Date.UTC(2014, 01, 01, 9, 15, 32, 123))
+    d: new Date(Date.UTC(2014, 1, 1, 9, 15, 32, 123))
 };
 
 var testObj = {
-	d: "2014-02-01T09:15:32.123Z"
+    d: "2014-02-01T09:15:32.123Z"
 };
 
-try
-{
-	var assert = require('assert');
-	
-	testObj = JSON.stringify(testObj, null, 2);
-	assert.equal(testObj, stringifyDate.stringify(obj, null, 2));
-	
-	testObj = stringifyDate.parse(testObj);
-	assert.ok(testObj.d instanceof Date);
-	
-	console.log('ok');
-}
-catch(e)
-{
-	console.log('fail', e);
+try {
+    var assert = require('assert');
+
+    testObj = JSON.stringify(testObj, null, 2);
+    assert.equal(testObj, stringifyDate.stringify(obj, null, 2));
+
+    testObj = stringifyDate.parse(testObj);
+    assert.ok(testObj.d instanceof Date);
+
+    console.log('ok');
+} catch (e) {
+    console.log('fail', e);
 }
