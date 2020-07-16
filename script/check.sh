8f90765c6de0ee49249dc14ca59b278f0c9e9d35
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2020-07-16 12:17:13 +0100

    Version 1.0.1

--- script/check.sh
+++ script/check.sh
@@ -1,4 +1,15 @@
 npm run build 2>&1 1>/dev/null
+
+if ! which git > /dev/null; then
+  echo "No git found, skipping"
+  exit 0
+fi
+
+if [ ! -d .git ]; then
+  echo "Not a git repo, skipping"
+  exit 0
+fi
+
 if ! git diff --exit-code -- browser.js > /dev/null ; then
   echo "You should run 'npm run build' before committing"
   exit 1
