165afc2e40998020c462213a1a939e435d8a7ded
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2021-03-09 10:31:47 +0000

    Do not parse strings with 4 digits (fixes #54)

--- test/test.js
+++ test/test.js
@@ -43,6 +43,11 @@ describe('JSON stringify date', function () {
             res.should.have.property('d');
             res.d.should.be.instanceof(Date);
         });
+        it('should not parse a 4 digit string', function () {
+            var res = JSONStringifyDate.parse('{"d":"10101"}');
+            res.should.have.property('d');
+            res.d.should.be.instanceof(String);
+        });
     });
     describe('#getReviver', function () {
         describe('without customization', function () {
